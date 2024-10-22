f57dad95aafcb3a65bbb4152b43070019bf64a16
tanha97 <137722129+tanha97@users.noreply.github.com>
2024-10-22 19:15:15 +0600

    object

---
+++ js_fundamental_2/basics6.js
@@ -0,0 +1,16 @@
+// Object is a collection of properties
+
+let person=
+{
+     firstName:'Tanha',
+     lastName :'Tahity'
+}
+
+console.log(person.firstName) //dot notation
+console.log(person['lastName'])// Array notation
+console.log(person.firstName= "Barsha") // update properties
+person.gender="Female"
+console.log(person) //Add properties
+delete person.gender
+console.log(person)
+
