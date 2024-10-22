63215a6995503d7520abb5eb0d06a14be3fefe9b
tanha97 <tanha.tahityn@gmail.com>
2024-10-22 23:26:34 +0600

    Adding function in object key

--- js_fundamental_2/basics6.js
+++ js_fundamental_2/basics6.js
@@ -3,14 +3,40 @@
 let person=
 {
      firstName:'Tanha',
-     lastName :'Tahity'
+     lastName :'Tahity',
+     age: 27,
+     fullName: function()
+     {
+          return this.firstName+" "+this.lastName
+
+     }
 }
 
+console.log(person.fullName())
 console.log(person.firstName) //dot notation
 console.log(person['lastName'])// Array notation
-console.log(person.firstName= "Barsha") // Update properties
+person.firstName= "Barsha" // Update properties
 person.gender="Female"
 console.log(person) //Add properties
 delete person.gender
 console.log(person) // Delete properties
+
+//Print all the value of the javascript object
+
+for(let key in person)
+{
+    
+    if(key=='fullName') 
+    {
+      console.log(person[key]())
+    }
+    
+    else
+    {
+     console.log(person[key])
+    }
+}
+
+
+
 
